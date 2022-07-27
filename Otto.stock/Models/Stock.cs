1b16480e9834476ef31ad050f27d597b48f155b4
Leo <leo.21@live.com.ar>
2022-07-27 16:49:15 -0300

    .

--- Otto.stock/Models/Stock.cs
+++ Otto.stock/Models/Stock.cs
@@ -6,30 +6,30 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string Origin { get; set; }
+        public int Quantity { get; set; }
 
-    //    public string SellerId { get; set; }
-    //    public string SellerIdMail?????? { get; set; }
-    //    public string MSellerId { get; set; }
-
-
-    //public string MItemId { get; set; }
-    //public string TSellerId { get; set; }
-
-    //    //pertenece a un carrito
-    //    public long PackId { get; set; }
-
-    //    public int Quantity { get; set; }
-
-    //    public string SKU { get; set; }
-
-    //    public string Category { get; set; }
+        //    public string SellerId { get; set; }
+        //    public string SellerIdMail?????? { get; set; }
+        //    public string MSellerId { get; set; }
 
 
-    //public string MShippingId { get; set; }
+        //public string MItemId { get; set; }
+        //public string TSellerId { get; set; }
 
-    //public string MBuyerId { get; set; }
-    //public string MSellerId { get; set; }
+        //    //pertenece a un carrito
+        //    public long PackId { get; set; }
 
 
-}
+        //    public string SKU { get; set; }
+
+        //    public string Category { get; set; }
+
+
+        //public string MShippingId { get; set; }
+
+        //public string MBuyerId { get; set; }
+        //public string MSellerId { get; set; }
+
+
+    }
 }
